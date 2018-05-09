16b1f653241665272bfb807abb1761a1c10ac5f5
Sara Hamilton <sara@indeterminate.net>
2018-05-09 15:42:45 -0700

    add controller tests folder

---
+++ MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MoneyMinder.Net.Models;
+using Moq;
+using System.Linq;
+using System;
+using MoneyMinder.Net.Controllers;
+using MoneyMinder.Net.Controllers;
+using MoneyMinder.Net.Tests.Models;
+
+namespace MoneyMinder.Net.Tests.ControllerTests
+{
+    class CategoryControllerTest
+    {
+    }
+}
