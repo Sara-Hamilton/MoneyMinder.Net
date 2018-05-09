bf0124b58f06034cd8195351e33e897a2c3048f1
Sara Hamilton <sara@indeterminate.net>
2018-05-09 16:04:53 -0700

    modify category controller test and test db context

--- MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
+++ MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
@@ -16,5 +16,33 @@ namespace MoneyMinder.Net.Tests.ControllerTests
         Mock<ICategoryRepository> mock = new Mock<ICategoryRepository>();
         EFCategoryRepository db = new EFCategoryRepository(new TestDbContext());
 
+        public void Dispose()
+        {
+            db.DeleteAll();
+        }
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Categories).Returns(new Category[]
+            {
+                new Category { CategoryId = 1, Name = "Clothing" },
+                new Category { CategoryId = 2, Name = "Savings" },
+                new Category { CategoryId = 3, Name = "Vacation" }
+            }.AsQueryable());
+        }
+
+        [TestMethod]
+        public void Mock_GetViewResultIndex_ActionResult()
+        {
+            //Arrange
+            DbSetup();
+            CategoryController controller = new CategoryController(mock.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
     }
 }
--- MoneyMinder.Net/Data/TestDbContext.cs
+++ MoneyMinder.Net/Data/TestDbContext.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyMinder.Net.Models;
+using MoneyMinder.Net.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace MoneyMinder.Net.Data
+namespace MoneyMinder.Net.Tests.Models
 {
-    public class TestDbContext
+    public class TestDbContext : MoneyDbContext
     {
+        public override DbSet<Category> Categories { get; set; }
+        public override DbSet<Fund> Funds { get; set; }
+        public override DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost; Port=8889; database=money_minder_net_test;uid=root;pwd=root;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            foreach (var relationship in modelbuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelbuilder);
+        }
     }
 }
