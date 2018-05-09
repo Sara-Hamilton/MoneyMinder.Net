dd29706c7bdbda7d7380332d58a5ac3e7288efeb
Sara Hamilton <sara@indeterminate.net>
2018-05-09 14:58:53 -0700

    add transaction repositories

---
+++ MoneyMinder.Net/Models/Repositories/EFTransactionRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoneyMinder.Net.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoneyMinder.Net.Models
+{
+    public class EFTransactionRepository : ITransactionRepository
+    {
+        MoneyDbContext db;
+        public EFTransactionRepository()
+        {
+            db = new MoneyDbContext();
+        }
+
+        public EFTransactionRepository(MoneyDbContext thisDb)
+        {
+            db = thisDb;
+        }
+
+        public IQueryable<Transaction> Transactions
+        { get { return db.Transactions; } }
+
+
+        public Transaction Save(Transaction transaction)
+        {
+            db.Transactions.Add(transaction);
+            db.SaveChanges();
+            return transaction;
+        }
+
+        public Transaction Edit(Transaction transaction)
+        {
+            db.Entry(transaction).State = EntityState.Modified;
+            db.SaveChanges();
+            return transaction;
+        }
+
+        public void Remove(Transaction transaction)
+        {
+            db.Transactions.Remove(transaction);
+            db.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            db.Transactions.RemoveRange(db.Transactions);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ MoneyMinder.Net/Models/Repositories/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyMinder.Net.Models
+{
+    public interface ITransactionRepository
+    {
+        IQueryable<Transaction> Transactions { get; }
+        Transaction Save(Transaction transaction);
+        Transaction Edit(Transaction transaction);
+        void Remove(Transaction transaction);
+        void DeleteAll();
+    }
+}
