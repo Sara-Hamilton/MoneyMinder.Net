7545f495a1c620dcdbc01c50c6706e6d11e72594
Sara Hamilton <sara@indeterminate.net>
2018-05-09 16:11:53 -0700

    add failing test condition for category controller index view test

--- MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
+++ MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
@@ -21,28 +21,29 @@ namespace MoneyMinder.Net.Tests.ControllerTests
             db.DeleteAll();
         }
 
-        private void DbSetup()
-        {
-            mock.Setup(m => m.Categories).Returns(new Category[]
-            {
-                new Category { CategoryId = 1, Name = "Clothing" },
-                new Category { CategoryId = 2, Name = "Savings" },
-                new Category { CategoryId = 3, Name = "Vacation" }
-            }.AsQueryable());
-        }
+        //private void DbSetup()
+        //{
+        //    mock.Setup(m => m.Categories).Returns(new Category[]
+        //    {
+        //        new Category { CategoryId = 1, Name = "Clothing" },
+        //        new Category { CategoryId = 2, Name = "Savings" },
+        //        new Category { CategoryId = 3, Name = "Vacation" }
+        //    }.AsQueryable());
+        //}
 
         [TestMethod]
         public void Mock_GetViewResultIndex_ActionResult()
         {
             //Arrange
-            DbSetup();
+            //DbSetup();
             CategoryController controller = new CategoryController(mock.Object);
 
             //Act
             var result = controller.Index();
 
             //Assert
-            Assert.IsInstanceOfType(result, typeof(ActionResult));
+            //Assert.IsInstanceOfType(result, typeof(ActionResult));
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
         }
     }
 }
