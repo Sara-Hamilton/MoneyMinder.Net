c2fdf98ce25781ac86f418751bebac0838650c89
Sara Hamilton <sara@indeterminate.net>
2018-05-09 09:35:46 -0700

    set up user controller

---
+++ MoneyMinder.Net/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using MoneyMinder.Net.Models;
+using MoneyMinder.Net.Data;
+
+namespace MoneyMinder.Net.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MoneyDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsertController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, MoneyDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
