fe81e46c92b14ff83dfc8be49faef011c6a23bcb
Sara Hamilton <sara@indeterminate.net>
2018-05-09 16:35:03 -0700

    add passing test condition for db starts at empty category controller test

--- MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
+++ MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
@@ -142,7 +142,7 @@ namespace MoneyMinder.Net.Tests.ControllerTests
             int result = db.Categories.ToList().Count;
 
             //Assert
-            Assert.AreEqual(1, result);
+            Assert.AreEqual(0, result);
         }
     }
 }
