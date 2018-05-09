4e4e494369b23fa68791d50f3fc30fd9e741125e
Sara Hamilton <sara@indeterminate.net>
2018-05-09 15:34:19 -0700

    add failing test conditions for category equals and constructor

---
+++ MoneyMinder.Net.Tests/Models/CategoryTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoneyMinder.Net.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoneyMinder.Net.Tests
+{
+    [TestClass]
+    public class CategoryTests
+    {
+        [TestMethod]
+        public void Equals_ProductsWithSameIdAreEqual_True()
+        {
+            //Arrange 
+            var category1 = new Category();
+            category1.CategoryId = 1;
+
+            var category2 = new Category();
+            category2.CategoryId = 2;
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(category1, category2);
+        }
+
+        [TestMethod]
+        public void Constructor_CreatesACategoryObject_True()
+        {
+            //Arrange
+            var category = new Category();
+
+            //Act
+
+            //Assert
+            Assert.IsInstanceOfType(category, typeof(Transaction));
+        }
+    }
+}
