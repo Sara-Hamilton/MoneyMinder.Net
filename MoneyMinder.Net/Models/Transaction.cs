ccba7c3220c4e501203104110e5cf8b0b7630905
Sara Hamilton <sara@indeterminate.net>
2018-05-09 13:50:07 -0700

    add transaction model

---
+++ MoneyMinder.Net/Models/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoneyMinder.Net.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal? Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal? PreviousTotal { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public int FundId { get; set; }
+        public virtual Fund Fund { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public Transaction() { }
+
+        public Transaction(string description, string type, DateTime date, decimal amount, int categoryId, int fundId, int id = 0)
+        {
+            Description = description;
+            Type = type;
+            Date = date;
+            Amount = amount;
+            CategoryId = categoryId;
+            FundId = fundId;
+        }
+
+        public override bool Equals(System.Object otherTransaction)
+        {
+            if (!(otherTransaction is Transaction))
+            {
+                return false;
+            }
+            else
+            {
+                Transaction newTransaction = (Transaction)otherTransaction;
+                return this.TransactionId.Equals(newTransaction.TransactionId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.TransactionId.GetHashCode();
+        }
+    }
+}
