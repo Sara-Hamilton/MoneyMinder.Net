86d7061ab3c7a978c109f33361f1d681402780a7
Sara Hamilton <sara@indeterminate.net>
2018-05-09 14:04:38 -0700

    add category controller

---
+++ MoneyMinder.Net/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoneyMinder.Net.Models;
+using MoneyMinder.Net.Data;
+
+namespace MoneyMinder.Net.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
