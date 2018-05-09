1dc0e820703d982e64cd1a30b4942bcd8691b39d
Sara Hamilton <sara@indeterminate.net>
2018-05-09 11:23:40 -0700

    modify user login

--- MoneyMinder.Net/Controllers/AccountController.cs
+++ MoneyMinder.Net/Controllers/AccountController.cs
@@ -47,5 +47,31 @@ namespace MoneyMinder.Net.Controllers
                 return View();
             }
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
