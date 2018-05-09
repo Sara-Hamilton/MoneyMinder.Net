7ab028ce1e18b687f55bb34a18f2e448193f60e1
Sara Hamilton <sara@indeterminate.net>
2018-05-09 15:11:07 -0700

    add CRUD functionality to category controller

--- MoneyMinder.Net/Controllers/CategoryController.cs
+++ MoneyMinder.Net/Controllers/CategoryController.cs
@@ -12,9 +12,81 @@ namespace MoneyMinder.Net.Controllers
 {
     public class CategoryController : Controller
     {
+        private ICategoryRepository categoryRepo;
+
+        public CategoryController(ICategoryRepository repo = null)
+        {
+            if (repo == null)
+            {
+                this.categoryRepo = new EFCategoryRepository();
+            }
+            else
+            {
+                this.categoryRepo = repo;
+            }
+        }
+
         public IActionResult Index()
         {
+            return View(categoryRepo.Categories.ToList());
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            categoryRepo.Save(category);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisCategory = categoryRepo.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisCategory = categoryRepo.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            categoryRepo.Edit(category);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisCategory = categoryRepo.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisCategory = categoryRepo.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+            categoryRepo.Remove(thisCategory);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAll(int id)
+        {
+            return View(categoryRepo.Categories.ToList());
+        }
+
+        [HttpPost, ActionName("DeleteAll")]
+        public IActionResult DeleteAllConfirmed(int id)
+        {
+            var categorys = categoryRepo.Categories;
+            categoryRepo.DeleteAll();
+            return RedirectToAction("Index");
         }
     }
 }
