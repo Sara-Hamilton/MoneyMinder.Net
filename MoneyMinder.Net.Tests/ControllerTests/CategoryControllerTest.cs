aabab2939efa2ae1e9a7cc0502c0ad9a5d9e161d
Sara Hamilton <sara@indeterminate.net>
2018-05-09 16:45:35 -0700

    add passing test condition for category controller delete all removes all categories from the database test

--- MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
+++ MoneyMinder.Net.Tests/ControllerTests/CategoryControllerTest.cs
@@ -189,7 +189,7 @@ namespace MoneyMinder.Net.Tests.ControllerTests
             db.DeleteAll();
 
             //Assert
-            Assert.AreEqual(1, db.Categories.Count());
+            Assert.AreEqual(0, db.Categories.Count());
         }
     }
 }
