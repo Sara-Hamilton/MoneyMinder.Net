c611fffe02f4c6e390b1ad24d779df7b78b205a5
Sara <sara@indeterminate.net>
2018-05-09 19:56:30 -0700

    add failing test condition for transaction equals test

---
+++ MoneyMinder.Net.Tests/Models/TransactionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoneyMinder.Net.Models;
+
+namespace MoneyMinder.Net.Tests.Models
+{
+    [TestClass]
+    public class TransactionTests
+    {
+        [TestMethod]
+        public void TransactionEquals_TransactionsWithSameIdAreEqual_True()
+        {
+            //Arrange 
+            var transaction1 = new Transaction();
+            transaction1.TransactionId = 1;
+
+            var transaction2 = new Transaction();
+            transaction2.TransactionId = 2;
+
+            //Act
+
+
+            //Assert
+            Assert.AreEqual(transaction1, transaction2);
+        }
+    }
+}
