590cf24043f810f79fd77005c1a4aa1d89fdf537
Sara Hamilton <sara@indeterminate.net>
2018-05-10 15:05:23 -0700

    add user total to user model

--- MoneyMinder.Net/Models/ApplicationUser.cs
+++ MoneyMinder.Net/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -8,5 +9,8 @@ namespace MoneyMinder.Net.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal? UserTotal { get; set; }
     }
 }
