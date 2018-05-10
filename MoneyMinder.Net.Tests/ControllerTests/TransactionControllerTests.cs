e1bafbc6ac8a944d961c5de8b3330383b66877c5
Sara <sara@indeterminate.net>
2018-05-09 20:47:58 -0700

    add transaction controller tests - problem setting up mock transactions

---
+++ MoneyMinder.Net.Tests/ControllerTests/TransactionControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MoneyMinder.Net.Models;
+using Moq;
+using System.Linq;
+using System;
+using MoneyMinder.Net.Controllers;
+using MoneyMinder.Net.Tests.Models;
+
+namespace MoneyMinder.Net.Tests.ControllerTests
+{
+    [TestClass]
+    public class TransactionControllerTests : IDisposable
+    {
+        Mock<ICategoryRepository> catMock = new Mock<ICategoryRepository>();
+        EFCategoryRepository catDb = new EFCategoryRepository(new TestDbContext());
+
+        Mock<IFundRepository> fundMock = new Mock<IFundRepository>();
+        EFFundRepository fundDb = new EFFundRepository(new TestDbContext());
+
+        Mock<ITransactionRepository> transactionMock = new Mock<ITransactionRepository>();
+        EFTransactionRepository transactionDb = new EFTransactionRepository(new TestDbContext());
+
+        public void Dispose()
+        {
+            catDb.DeleteAll();
+            fundDb.DeleteAll();
+            transactionDb.DeleteAll();
+        }
+
+        private void DbSetup()
+        {
+            catMock.Setup(m => m.Categories).Returns(new Category[]
+            {
+                new Category { CategoryId = 1, Name = "Clothing" },
+                new Category { CategoryId = 2, Name = "Savings" },
+                new Category { CategoryId = 3, Name = "Vacation" }
+            }.AsQueryable());
+
+            fundMock.Setup(m => m.Funds).Returns(new Fund[]
+            {
+                new Fund { FundId = 1, Name = "House Account" },
+                new Fund { FundId = 2, Name = "Escrow" },
+                new Fund { FundId = 3, Name = "Savings" },
+            }.AsQueryable());
+
+            //var testDate = new DateTime(2018, 03, 01);
+            //transactionMock.Setup(m => m.Transactions).Returns(new Transaction[]
+            //{
+            //    new Transaction { TransactionId = 1, Description = "Paycheck", Type = "Deposit", Date = testDate, Amount = 523.72m, CategoryId = 1, FundId = 1}
+            //}).AsQueryable());
+        }
+    }
+}
