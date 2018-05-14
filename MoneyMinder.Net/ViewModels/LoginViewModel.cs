52acb79d33c0935f9bf5e3a4bf779c6abbb118ae
Sara Hamilton <sara@indeterminate.net>
2018-05-14 08:13:21 -0700

    modify login view model

--- MoneyMinder.Net/ViewModels/LoginViewModel.cs
+++ MoneyMinder.Net/ViewModels/LoginViewModel.cs
@@ -7,7 +7,11 @@ namespace MoneyMinder.Net.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [Display(Name = "Name")]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
