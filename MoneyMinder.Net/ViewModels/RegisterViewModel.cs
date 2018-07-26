3a5850c5f4c5c18698ca89b3c41851c513dc78c5
Sara <sara@indeterminate.net>
2018-07-26 10:10:45 -0700

    add error messages at registration if name or password are too short

--- MoneyMinder.Net/ViewModels/RegisterViewModel.cs
+++ MoneyMinder.Net/ViewModels/RegisterViewModel.cs
@@ -9,10 +9,12 @@ namespace MoneyMinder.Net.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [Display(Name = "Name")]
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
